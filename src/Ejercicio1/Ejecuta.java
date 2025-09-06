15e5df8e5be07cc3003bcd155a3651b8e53b1942
Misabel <isabelsalguedo19@gmail.com>
2025-09-05 22:33:37 -0500

    feat(Inicializando objetos) Falta añadir la forma para ingresar datos a los libros por teclado

---
+++ src/Ejercicio1/Ejecuta.java
@@ -0,0 +1,14 @@
+package Ejercicio1;
+
+public class Ejecuta {
+    public static void main(String[] args) {
+        Libro L1 = new Libro();
+        L1.mostrarvalores();
+        System.out.println("Libro inicializado con un constructor por defecto");
+
+        Libro L2 = new Libro("Corazón delator", "Edgar Allan Poe",20);
+        L2.mostrarvalores();
+        System.out.println("Libro inicializado con un constructor parametrizado");
+
+    }
+}
--- src/Ejercicio1/Libro.java
+++ src/Ejercicio1/Libro.java
@@ -1,5 +1,56 @@
 package Ejercicio1;
+//Crea una clase Libro con las siguientes propiedades: titulo, autor, numeroPaginas. Define:
+//1. Un constructor por defecto que asigne valores predeterminados.
+//2. Un constructor parametrizado que permita asignar valores específicos.
+//3. Métodos para mostrar los detalles del libro.
+
+import javax.swing.*;
 
 public class Libro {
+    private String titulo;
+    private String autor;
+    private int numerodepaginas;
 
+    public Libro(){
+        titulo = "Desconocido";
+        autor = "Desconocido";
+        numerodepaginas = 0;
+    }
+
+    public Libro(String titulo, String autor, int numerodepaginas){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numerodepaginas = numerodepaginas;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getNumerodepaginas() {
+        return numerodepaginas;
+    }
+
+    public void setNumerodepaginas(int numerodepaginas) {
+        this.numerodepaginas = numerodepaginas;
+    }
+
+    public void mostrarvalores(){
+        System.out.println("El titulo del libro es: "+ titulo);
+        System.out.println("El nombre del autor es: "+ autor);
+        System.out.println("El numero de páginas es: "+ numerodepaginas);
+
+    }
 }
